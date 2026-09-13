08e17112dfc472df207dcf260d42e1887b2d0f85
robertd2000 <turgamobil@gmail.com>
2026-09-13 18:31:50 +0300

    getline

--- db.c
+++ db.c
@@ -20,8 +20,6 @@ InputBuffer *new_input_buffer() {
 
 void print_prompt() { printf("db > "); }
 
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-
 void read_input(InputBuffer *input_buffer) {
   ssize_t bytes_read =
       getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
