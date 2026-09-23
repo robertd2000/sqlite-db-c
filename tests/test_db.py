e2f0fa488a4069c8643e7c899e9ce0c7e8060518
robertd2000 <turgamobil@gmail.com>
2026-09-23 18:32:53 +0300

    tree

--- tests/test_db.py
+++ tests/test_db.py
@@ -206,9 +206,9 @@ def test_prints_constants(tmp_path):
         "db > Constants:",
         "ROW_SIZE: 293",
         "COMMON_NODE_HEADER_SIZE: 6",
-        "LEAF_NODE_HEADER_SIZE: 10",
+        "LEAF_NODE_HEADER_SIZE: 14",
         "LEAF_NODE_CELL_SIZE: 297",
-        "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+        "LEAF_NODE_SPACE_FOR_CELLS: 4082",
         "LEAF_NODE_MAX_CELLS: 13",
         "db > ",
     ]
@@ -288,5 +288,37 @@ def test_insert_into_internal_node(tmp_path):
 
     assert result[-2:] == [
         "db > Executed.",
+        "db > ",
+    ]
+
+
+def test_prints_all_rows_in_a_multi_level_tree(tmp_path):
+    db_file = tmp_path / "test.db"
+
+    script = [
+        *[f"insert {i} user{i} person{i}@example.com" for i in range(1, 16)],
+        "select",
+        ".exit",
+    ]
+
+    result = run_script(script, str(db_file))
+
+    assert result[15:] == [
+        "db > (1, user1, person1@example.com)",
+        "(2, user2, person2@example.com)",
+        "(3, user3, person3@example.com)",
+        "(4, user4, person4@example.com)",
+        "(5, user5, person5@example.com)",
+        "(6, user6, person6@example.com)",
+        "(7, user7, person7@example.com)",
+        "(8, user8, person8@example.com)",
+        "(9, user9, person9@example.com)",
+        "(10, user10, person10@example.com)",
+        "(11, user11, person11@example.com)",
+        "(12, user12, person12@example.com)",
+        "(13, user13, person13@example.com)",
+        "(14, user14, person14@example.com)",
+        "(15, user15, person15@example.com)",
+        "Executed.",
         "db > ",
     ]
